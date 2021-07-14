234093efba685944623952d13824c96e7c5212b9
jaedango <javier.andrews@ug.uchile.cl>
2021-07-13 23:48:28 -0400

    Se agregan variables al proyecto, entrega final final (No habia leido que tenia que agregarlas para la entrega 2)

--- src/main/java/files/operations/constant/ConstantFactory.java
+++ src/main/java/files/operations/constant/ConstantFactory.java
@@ -140,4 +140,48 @@ public class ConstantFactory {
         return constantLogical;
     }
 
+    // Variables
+
+    /**
+     * Variables values -> can be String, float, int, binary or boolean
+     * @param name -> String representing the variable name
+     * @param value -> Value of the variable
+     */
+    public ConstantNum variable(String name, int value) {
+        ConstantNum constantNum = new ConstantNum(new ScInt(value));
+        hashTable.put(name, constantNum);
+        return constantNum;
+    }
+
+    public ConstantNum variable(String name, float value) {
+        ConstantNum constantNum = new ConstantNum(new ScFloat(value));
+        hashTable.put(name, constantNum);
+        return constantNum;
+    }
+
+    public Constant variable(String name, String value) {
+        if (value.matches("^[01]+$")) {
+            ConstantNum constantNum = new ConstantNum(new ScBinary(value));
+            hashTable.put(name, constantNum);
+            return constantNum;
+        }
+        else {
+            ConstantString constantString = new ConstantString(new ScString(value));
+            hashTable.put(name, constantString);
+            return constantString;
+        }
+    }
+
+    public ConstantLogical variable(String name, boolean value) {
+        ConstantLogical constantLogical = new ConstantLogical(new ScBoolean(value));
+        hashTable.put(name, constantLogical);
+        return constantLogical;
+    }
+
+    public Constant variable(String value) {
+        Constant constant = hashTable.get(value);
+        return constant;
+    }
+
+
 }
